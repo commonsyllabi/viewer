311172ffcd9ce7126b8be13e3a8f451078de8cf9
période <pierre.depaz@gmail.com>
2022-03-28 17:00:24 +0200

    added tests for all test_files

--- viewer/viewer_test.go
+++ viewer/viewer_test.go
@@ -2,6 +2,9 @@ package viewer
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -12,8 +15,8 @@ const imscc_path = "../test_files/canvas_large_1.3.imscc"
 // check go test tables
 
 func TestLoadEmpty(t *testing.T) {
-	var cc Cartridge //-- declare interface instance
-	cc = NewIMSCC()  //-- assign underlying value
+	var cc Cartridge
+	cc = NewIMSCC() //-- assign underlying value
 	_, err := cc.Load("")
 
 	if err == nil {
@@ -40,8 +43,8 @@ func TestParseManifest(t *testing.T) {
 }
 
 func TestLoadCorrect(t *testing.T) {
-	var cc Cartridge //-- declare interface instance
-	cc = NewIMSCC()  //-- assign underlying value
+	var cc Cartridge
+	cc = NewIMSCC() //-- assign underlying value
 	cc, err := cc.Load(imscc_path)
 
 	if err != nil {
@@ -56,8 +59,90 @@ func TestLoadCorrect(t *testing.T) {
 	}
 }
 
+func TestLoadAll(t *testing.T) {
+	cwd, _ := os.Getwd()
+	files, err := ioutil.ReadDir(filepath.Join(cwd, "../test_files"))
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	fmt.Printf("Test loading %d cartridges\n", len(files))
+	var i int = 0
+	for _, file := range files {
+		i++
+		if file.IsDir() {
+			continue
+		}
+
+		var cc Cartridge
+		cc = NewIMSCC() //-- assign underlying value
+		cc, err := cc.Load(filepath.Join("../test_files", file.Name()))
+
+		if err != nil {
+			fmt.Println(err)
+			t.Fail()
+		}
+
+		var empty IMSCC
+		if reflect.DeepEqual(cc, empty) {
+			fmt.Printf("Expecting struct type, got: %v", reflect.TypeOf(cc).Kind())
+			t.Fail()
+		}
+
+		fmt.Printf("Parsed %d/%d - %s\n", i, len(files), cc.Title())
+	}
+}
+
+func TestParseManifestAll(t *testing.T) {
+	cwd, _ := os.Getwd()
+	files, err := ioutil.ReadDir(filepath.Join(cwd, "../test_files"))
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	fmt.Printf("testing %d cartridges\n", len(files))
+	var i int = 0
+	for _, file := range files {
+		i++
+		if file.IsDir() {
+			continue
+		}
+
+		var cc Cartridge
+		cc = NewIMSCC() //-- assign underlying value
+		cc, err := cc.Load(filepath.Join("../test_files", file.Name()))
+
+		if err != nil {
+			fmt.Println(err)
+			t.Fail()
+		}
+
+		var empty IMSCC
+		if reflect.DeepEqual(cc, empty) {
+			fmt.Printf("Expecting struct type, got: %v", reflect.TypeOf(cc).Kind())
+			t.Fail()
+		}
+
+		manifest, err := cc.ParseManifest()
+
+		if err != nil {
+			fmt.Println(err)
+			t.Fail()
+		}
+
+		if reflect.TypeOf(manifest).Kind() != reflect.Struct {
+			fmt.Printf("Expecting struct type, got: %v", reflect.TypeOf(manifest).Kind())
+			t.Fail()
+		}
+	}
+}
+
 func TestDump(t *testing.T) {
-	var cc Cartridge //-- declare interface instance
+	var cc Cartridge
 	cc = NewIMSCC()
 	cc, _ = cc.Load(imscc_path)
 
