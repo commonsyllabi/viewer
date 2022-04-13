c9f17d8de9d4eaf78abcab5fe0ce4fd1c6df0617
période <pierre.depaz@gmail.com>
2022-04-13 17:38:25 +0200

    dockerized connection to db

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-const DATABASE_URL = "postgres://postgres@localhost:5432/testdatabase"
+const DATABASE_URL = "postgres://postgres:postgres@db:5432/postgres"
 
 func Connect() {
 	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
@@ -20,25 +20,4 @@ func Connect() {
 	defer conn.Close(context.Background())
 
 	zero.Log.Info().Msgf("Connected to database: %v", DATABASE_URL)
-
-	rows, err := conn.Query(context.Background(), "select * from testtable")
-	if err != nil {
-		zero.Log.Error().Msgf("Error getting row: %v", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var num int
-		var name string
-
-		if err := rows.Scan(&num, &name); err != nil {
-			zero.Log.Error().Msgf("Error looping over row: %v", err)
-		}
-
-		zero.Log.Info().Msgf("reading name %s and num %d", name, num)
-	}
-
-	if err := rows.Err(); err != nil {
-		zero.Log.Error().Msgf("Error final over row: %v", err)
-	}
 }
--- internal/main.go
+++ internal/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/commonsyllabi/viewer/internal/api"
+	"github.com/commonsyllabi/viewer/internal/db"
 	zero "github.com/commonsyllabi/viewer/internal/logger"
 )
 
 func main() {
 	zero.InitLog(true)
 	zero.Log.Info().Msg("Starting Cosyl")
-	// db.Connect()
+
+	db.Connect()
 	api.StartServer()
 }
