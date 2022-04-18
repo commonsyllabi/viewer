5d19dcfce59c5de52498a4322e5d396f2fabcd06
période <pierre.depaz@gmail.com>
2022-04-18 16:39:58 +0200

    added global vue for cartridge display

--- internal/web/www/js/script.js
+++ internal/web/www/js/script.js
@@ -1,21 +1,34 @@
-let upload = () => {
-    let form = document.getElementById("upload-form")
-    let formData = new FormData(form)
-
-    if(formData.get("cartridge").name == ""){
-        console.warn("can't submit an empty cartridge!");
-        return
-    }
-
-    fetch("/api/upload",{
-        method: 'POST',
-        body: formData
-    }).then(res => {
-        return res.json()
-    }).then(data => {
-        console.log(data);
-        document.getElementById("log").innerText = `Succesfully loaded: ${data.Metadata.Lom.General.Title.String.Text}`
-    }).catch(err => {
-        console.error(err);
-    })
+let init = () => {
+    Vue.createApp({
+        data() {
+            return {
+                cartridge : null
+            }
+        },
+        methods: {
+            upload() {
+                let form = document.getElementById("upload-form")
+                let formData = new FormData(form)
+            
+                if(formData.get("cartridge").name == ""){
+                    console.warn("can't submit an empty cartridge!");
+                    return
+                }
+            
+                fetch("/api/upload",{
+                    method: 'POST',
+                    body: formData
+                }).then(res => {
+                    return res.json()
+                }).then(data => {
+                    console.log(data);
+                }).catch(err => {
+                    console.error(err);
+                })
+            }
+        },
+        mounted() {
+            this.cartridge = DATA
+        }
+    }).mount('#app')
 }
