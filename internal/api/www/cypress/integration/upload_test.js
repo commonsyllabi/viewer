eaba8367fc761d04db2daf67c06f5d0e0b62a74b
période <pierre.depaz@gmail.com>
2022-04-26 23:19:22 +0200

    added cypress frontend test suite

---
+++ internal/api/www/cypress/integration/upload_test.js
@@ -0,0 +1,18 @@
+// 1. set up app state
+// 2. take an action
+// 3. make an assertion about the resulting state
+
+describe('Cartridge page', () => {
+    it('uploads a cartridge', () => {
+        cy.intercept('POST', '/api/upload', { fixture: 'test_01_upload_response.json' }).as('uploadCartridge')
+        cy.visit('http://localhost:3000')
+
+        cy.get('#upload-file').selectFile("cypress/fixtures/test_01.imscc")
+        cy.get('#upload-submit').click()
+
+        cy.get('#log').contains("uploading")
+
+        cy.wait('@uploadCartridge')
+        cy.get('#log').contains("uploaded")
+    })
+})
