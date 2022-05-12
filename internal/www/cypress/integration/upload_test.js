17d9b2ee826a20cca869db6c42608b3dbd679670
période <pierre.depaz@gmail.com>
2022-05-13 00:04:21 +0200

    test submit course front end

--- internal/www/cypress/integration/upload_test.js
+++ internal/www/cypress/integration/upload_test.js
@@ -3,9 +3,9 @@
 // 3. make an assertion about the resulting state
 
 describe('Cartridge page', () => {
-    it('uploads a cartridge', () => {
-        cy.intercept('POST', '/api/upload', { fixture: 'test_01_upload_response.json' }).as('uploadCartridge')
-        cy.visit('/cartridge.html')
+  it('uploads a cartridge', () => {
+    cy.intercept('POST', '/api/upload', { fixture: 'test_01_upload_response.json' }).as('uploadCartridge')
+    cy.visit('/cartridge.html')
 
     cy.get("#upload-file").selectFile("cypress/fixtures/test_01.imscc");
     cy.get("#upload-submit").click();
@@ -15,4 +15,13 @@ describe('Cartridge page', () => {
     cy.wait("@uploadCartridge");
     cy.get("#log").contains("uploaded");
   });
+
+  it('fills in the email', () => {
+    cy.get("#email").type("pierre.depaz@gmail.com")
+    cy.get("#email-conf").type("pierre.depaz@gmail.com")
+  });
+
+  it('submits a new course', () => {
+    cy.get("#course-submit")
+  })
 });
