426e8727be4521a36d9cd32600f21b88c89592c5
période <pierre.depaz@gmail.com>
2022-05-17 20:29:42 +0200

    added action buttons on cartridge component

--- www/src/Cartridge.vue
+++ www/src/Cartridge.vue
@@ -1,7 +1,8 @@
 <template>
-<Header></Header>
+  <Header></Header>
   <main class="container p-3">
-    <div class="container pt-4 mb-3 border rounded" v-if="isUploaded">
+    <!-- this should be moved to a modal -->
+    <div class="container pt-4 mb-3 border rounded" hidden>
       <form id="submit-form" action="/syllabi/" method="POST">
         <div>
           <label for="title">Title of the course</label>
@@ -23,106 +24,88 @@
           <input type="email" name="email-conf" id="email-conf" />
         </div>
 
-        <button
-          id="course-submit"
-          type="button"
-          class="btn btn-primary mb-4"
-          @click="submit()"
-        >submit</button>
+        <button id="course-submit" type="button" class="btn btn-primary mb-4" @click="submit()">submit</button>
       </form>
     </div>
     <!-- upload form -->
     <div class="container pt-4 mb-3 border rounded">
       <form id="upload-form" action="/api/upload" method="post">
         <div class="form-group">
-          <label
-            for="cartridgeInput"
-            class="d-block h5 mb-3"
-          >Upload a common cartridge (.imscc) file</label>
-          <input
-            id="upload-file"
-            type="file"
-            name="cartridge"
-            class="form-control-file d-block mb-2"
-          />
-          <button
-            id="upload-submit"
-            type="button"
-            class="btn btn-primary mb-3"
-            @click="upload()"
-          >upload</button>
+          <label for="cartridgeInput" class="d-block h5 mb-3">Upload a common cartridge (.imscc) file</label>
+          <input id="upload-file" type="file" name="cartridge" class="form-control-file d-block mb-2" />
+          <button id="upload-submit" type="button" class="btn btn-primary mb-3" @click="upload()">upload</button>
         </div>
       </form>
     </div>
 
     <!-- status log -->
-    <div id="log" class="container py-4 mb-3 border rounded text-light bg-dark">
+    <div id="log" class="container py-1 mb-3">
       <pre>{{ log }}</pre>
     </div>
 
-    <!-- metadata viewer -->
-    <div class="row mb-3">
-      <div id="metadata-accordion" class="accordion">
-        <div class="accordion-item">
-          <h2 id="headingOne" class="accordion-header">
-            <button
-              class="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseMeta"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >Cartridge Metadata</button>
-          </h2>
-          <div
-            id="collapseMeta"
-            class="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#metadata-accordion"
-          >
-            <div class="accordion-body">
-              <!-- file metadata placeholder -->
-              <div v-if="!isUploaded" class="metadata-placeholder">
-                <span class="text-muted">
-                  <em>metadata goes here</em>
-                </span>
-              </div>
 
-              <!-- file metadata -->
-              <div v-if="isUploaded" class="metadata">
-                <div class="title">{{ manifest.Metadata.Lom.General.Title.String.Text }}</div>
+    <div v-if="isUploaded" class="container cartridge">
+    <!-- actions -->
+    <div class="row mb-2 actions">
+      <button type="button" class="action" @click="showMetadata = !showMetadata">{{ 
+      showMetadata ? "hide metadata & index" : "show metadata & index"
+      }}</button>
+      <button type="button" class="action" @click="reset()">load another file</button>
+      <button type="button" class="action">upload this file</button>
+    </div>
+      <!-- metadata viewer -->
+      <div class="row mb-3" v-if="showMetadata">
+        <div id="metadata-accordion" class="accordion">
+          <div class="accordion-item">
+            <h2 id="headingOne" class="accordion-header">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMeta"
+                aria-expanded="true" aria-controls="collapseOne">Cartridge Metadata</button>
+            </h2>
+            <div id="collapseMeta" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+              data-bs-parent="#metadata-accordion">
+              <div class="accordion-body">
+                <!-- file metadata placeholder -->
+                <div v-if="!isUploaded" class="metadata-placeholder">
+                  <span class="text-muted">
+                    <em>metadata goes here</em>
+                  </span>
+                </div>
+
+                <!-- file metadata -->
+                <div v-if="isUploaded" class="metadata">
+                  <div class="title">{{ manifest.Metadata.Lom.General.Title.String.Text }}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- viewer -->
-    <!-- <div v-if="isUploaded" class="container cartridge"> -->
-    <div class="container border rounded mb-5 cartridge-viewer">
-      <!-- file navigator -->
-      <div class="row">
-        <!-- items panel -->
-        <div class="col-6 overflow-scroll items-panel">
-          <h6 class="my-2">Items Who Knows Index</h6>
+      <!-- viewer -->
+      <div class="container border rounded mb-5 cartridge-viewer">
+        <!-- file navigator -->
+        <div class="row">
+          <!-- items panel -->
+          <div class="col-6 overflow-scroll items-panel">
+            <h6 class="my-2">Items Who Knows Index</h6>
 
-          <!-- items listing -->
-          <!-- TODO: dont use index, bad prac -->
-          <div v-for="(i, index) in items" :key="index">
-            <Item :item="i" :cartridge="cartridge.name" />
-            <hr />
+            <!-- items listing -->
+            <!-- TODO: dont use index, bad prac -->
+            <div v-for="(i, index) in items" :key="index">
+              <Item :item="i" :cartridge="cartridge.name" />
+              <hr />
+            </div>
           </div>
-        </div>
 
-        <!-- resources panel -->
-        <div class="col-6 overflow-scroll resources-panel">
-          <h6 class="my-2">Resources Index</h6>
+          <!-- resources panel -->
+          <div class="col-6 overflow-scroll resources-panel">
+            <h6 class="my-2">Resources Index</h6>
 
-          <!-- resources listing -->
-          <!-- TODO: dont use index, bad prac -->
-          <div v-for="(r, index) in resources" :key="index" class>
-            <Resource :resource="r" :cartridge="cartridge.name" />
+            <!-- resources listing -->
+            <!-- TODO: dont use index, bad prac -->
+            <div v-for="(r, index) in resources" :key="index" class>
+              <Resource :resource="r" :cartridge="cartridge.name" />
+            </div>
           </div>
         </div>
       </div>
@@ -154,6 +137,8 @@ let cartridge = reactive({ name: "" });
 let manifest = new Object() as ManifestType;
 let items = new Array<ItemType>()
 let resources = new Array<ResourceType>()
+
+let showMetadata = ref(false);
 
 let log = ref("ready");
 let isUploaded = ref(false);
@@ -202,9 +187,20 @@ let upload = function () {
     });
 };
 
+let reset = () => {
+  isUploaded.value = false
+  showMetadata.value = false
+  
+  Object.assign(manifest, {})
+  Object.assign(items, {})
+  Object.assign(resources, {})
+
+  log.value = "reset cartridge"
+}
+
 let submit = () => {
 
-  if (isInvalidEmail()){
+  if (isInvalidEmail()) {
     log.value = "please make sure that the emails are matching!"
     return
   }
@@ -295,13 +291,31 @@ let validateSubmission = (_data: FormData) => {
 .cartridge-viewer {
   // border: 1px solid orange;
   height: 75vh;
+
   .items-panel {
     // border: 1px solid blue;
     height: 75vh;
   }
+
   .resources-panel {
     // border: 1px solid red;
     height: 75vh;
+  }
+}
+
+.actions {
+  display: flex;
+
+  .action{
+    width: max-content;
+    border: none;
+    background-color: white;
+    color: grey;
+    text-decoration: underline;
+  }
+
+  .action:hover{
+    color: black;
   }
 }
 </style>
