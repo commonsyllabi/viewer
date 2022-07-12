08592f77d0b039be9e3a1db1d30fc607938d88bf
pat shiu <shiu.patricia@gmail.com>
2022-07-11 21:09:52 -0400

    feat: Display index item details

--- www/src/components/Item.vue
+++ www/src/components/Item.vue
@@ -1,61 +1,97 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import Resource from "./Resource.vue";
-import { ItemType } from "../js/types";
+  import { ref, computed } from 'vue'
+  import Resource from './Resource.vue'
+  import { ItemType } from '../js/types'
 
-const props = defineProps<{
-  item: ItemType,
-  cartridge: string
-}>();
+  const props = defineProps<{
+    item: ItemType
+    cartridge: string
+  }>()
 
-const showResources = ref(true);
+  const showResources = ref(true)
 </script>
 
 <template>
-  <div class="item-title">{{ props.item.Item.Title }}</div>
+  <h6 class="item-title fs-6 fw-bold">
+    {{ props.item.Item.Title }}
+  </h6>
   <div class="item-id">{{ props.item.Item.Identifier }}</div>
-
-  <div v-if="props.item.Children">
-    <h6 class="fs-6 text-uppercase">
-      Children:
-    </h6>
-    <div v-for="child in props.item.Children" class="sub-item">
-      <div>{{ child.Item.Identifier }} - {{ child.Item.Title }}</div>
+  <div class="item-children-container">
+    <div v-if="props.item.Children" class="ps-2">
+      <div
+        v-for="child in props.item.Children"
+        :key="child.Item.Identifier"
+        class="sub-item px-2 py-1 m-1"
+      >
+        <div>
+          <div>
+            {{ child.Item.Title }}
+          </div>
+          <div class="item-id">
+            {{ child.Item.Identifier }}
+          </div>
+          <div v-if="child.Item.Identifierref" class="item-ref">
+            Refers to:
+            <span class="text-break">{{ child.Item.Identifierref }}</span>
+          </div>
+          <div v-if="child.Resources" class="item-type">
+            resource type:
+            <span>
+              {{ child.Resources[0].Type }}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
   <ul v-if="showResources" class="item-resources">
-    <li class="border rounded p-1 m-1 res" v-for="res in props.item.Resources">
+    <li
+      class="border rounded p-1 m-1 res"
+      v-for="res in props.item.Resources"
+      :key="res.Identifier"
+    >
       <!-- <Resource :resource="res" :cartridge="cartridge" /> -->
-      <div class="res-title"> {{ res.Title }}</div>
-      <div class="res-id"> {{ res.Identifier }}</div>
-      <div class="res-type"> {{ res.Type }}</div>
+      <div class="res-title">{{ res.Title }}</div>
+      <div class="res-id">{{ res.Identifier }}</div>
+      <div class="res-type">{{ res.Type }}</div>
     </li>
   </ul>
 </template>
 
-<style scoped>
-.resources {
-  cursor: pointer;
-}
+<style lang="scss" scoped>
+  @import '../css/global-vars.scss';
+  .resources {
+    cursor: pointer;
+  }
 
-.item-id{
-  color: lightgrey;
-  font-size: 0.8em;
-}
+  .item-id,
+  .item-ref,
+  .item-type {
+    color: $secondary;
+    font-size: 0.8em;
+  }
 
-.item-resources{
-  padding-left: 5px;
-  border-left: 1px solid black;
-}
+  .item-resources {
+    padding-left: 5px;
+    border-left: 1px solid black;
+  }
 
-.res{
-  list-style: none;
-  font-size: 0.7em;
-}
+  .item-children-container {
+    border-left: 1px solid $secondary;
+  }
 
-.res-id{
-  color: lightgray;
-}
+  .sub-item {
+    border: 2px solid $gray-200;
+    border-radius: $border-radius-sm;
+  }
 
+  .res {
+    list-style: none;
+    font-size: 0.7em;
+  }
+
+  .res-id {
+    color: lightgray;
+  }
 </style>
